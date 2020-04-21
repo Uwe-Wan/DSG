116a771481c8f09ad27edb5697ffcbc5af2eca1a
Uwe <stochastikgott@web.de>
2020-04-21 20:27:06 +0200

    Added converter which checks if an object is null and sets a boolean to true if its not

---
+++ DSG/DSG.Presentation/Converter/ObjectSelectedToBooleanConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace DSG.Presentation.Converter
+{
+    public class ObjectSelectedToBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return "This is not needed";
+        }
+
+    }
+}
