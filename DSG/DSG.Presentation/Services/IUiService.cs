f4281737ed7856f070eb1d90b6d5841b0502e3bc
Uwe <stochastikgott@web.de>
2020-04-24 22:01:06 +0200

    added UI Service

---
+++ DSG/DSG.Presentation/Services/IUiService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSG.Presentation.Services
+{
+    public interface IUiService
+    {
+        void ShowMessageBox(string message);
+
+        void ShowErrorMessage(string message, string caption);
+
+        bool AskForConfirmationMessage(string message, string caption);
+
+        bool ShowWarningMessage(string message, string caption);
+    }
+}
---
+++ DSG/DSG.Presentation/Services/UiService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DSG.Presentation.Services
+{
+    public class UiService : IUiService
+    {
+        public void ShowMessageBox(string message)
+        {
+            MessageBox.Show(message);
+        }
+
+        public void ShowErrorMessage(string message, string caption)
+        {
+            MessageBoxButton buttons = MessageBoxButton.OK;
+            MessageBoxImage icon = MessageBoxImage.Error;
+            MessageBox.Show(message, caption, buttons, icon);
+        }
+
+        public bool AskForConfirmationMessage(string message, string caption)
+        {
+            MessageBoxButton buttons = MessageBoxButton.YesNo;
+            MessageBoxImage icon = MessageBoxImage.Exclamation;
+            return MessageBox.Show(message, caption, buttons, icon) == MessageBoxResult.Yes;
+        }
+
+        public bool ShowWarningMessage(string message, string caption)
+        {
+            MessageBoxButton buttons = MessageBoxButton.OKCancel;
+            MessageBoxImage icon = MessageBoxImage.Warning;
+            return MessageBox.Show(message, caption, buttons, icon) == MessageBoxResult.OK;
+        }
+    }
+}
