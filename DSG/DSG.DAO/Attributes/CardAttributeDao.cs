bc097a90c1cae6ab70749842ab09751753852db8
Uwe <stochastikgott@web.de>
2020-05-09 13:33:12 +0200

    added DAO to insert card attributes

---
+++ DSG/DSG.DAO/Attributes/CardAttributeDao.cs
@@ -0,0 +1,28 @@
+﻿using DSG.BusinessEntities;
+using DSG.BusinessEntities.CardAttributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSG.DAO.Attributes
+{
+    public class CardAttributeDao : ICardAttributeDao
+    {
+        private CardManagementDbContext _ctx;
+
+        public CardManagementDbContext Ctx
+        {
+            get { return _ctx; }
+            set { _ctx = value; }
+        }
+
+        public void InsertAttribute(CardAttribute attribute)
+        {
+            Ctx.CardAttribute.Add(attribute);
+
+            Ctx.SaveChanges();
+        }
+    }
+}
---
+++ DSG/DSG.DAO/Attributes/ICardAttributeDao.cs
@@ -0,0 +1,9 @@
+﻿using DSG.BusinessEntities.CardAttributes;
+
+namespace DSG.DAO.Attributes
+{
+    public interface ICardAttributeDao
+    {
+        void InsertAttribute(CardAttribute cardAttribute);
+    }
+}
