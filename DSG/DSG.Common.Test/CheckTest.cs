5924dac8013efd3d3d5f872821e5fdd4220ad901
Uwe <stochastikgott@web.de>
2020-09-10 15:54:50 +0200

    DSG-3: added log4net alongside a Check class to check whether properties have been injected and objects are not null

---
+++ DSG/DSG.Common.Test/CheckTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using DSG.Common.Exceptions;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DSG.Common.Test
+{
+    [TestFixture]
+    public class CheckTest
+    {
+        [Test]
+        public void RequireInjected_NotInjected_ThrowsException()
+        {
+            //Arrange
+            string objectToTest = null;
+            string parent = "Parent";
+
+            //Act
+            Action act = () => Check.RequireInjected(objectToTest, nameof(objectToTest), parent);
+
+            //Assert
+            act.Should().Throw<TechnicalException>().WithMessage("The objectToTest was not correctly injected into Parent.");
+        }
+
+        [Test]
+        public void RequireInjected_Injected_NoException()
+        {
+            //Arrange
+            string objectToTest = "NotNull";
+            string parent = "Parent";
+
+            //Act
+            Action act = () => Check.RequireInjected(objectToTest, nameof(objectToTest), parent);
+
+            //Assert
+            act.Should().NotThrow<TechnicalException>();
+        }
+
+        [Test]
+        public void RequireNotNull_Null_ThrowsException()
+        {
+            //Arrange
+            string objectToTest = null;
+            string parent = "Parent";
+
+            //Act
+            Action act = () => Check.RequireNotNull(objectToTest, nameof(objectToTest), parent);
+
+            //Assert
+            act.Should().Throw<TechnicalException>().WithMessage("The objectToTest in Parent is null but must not be.");
+        }
+
+        [Test]
+        public void RequireNotNull_NotNull_NoException()
+        {
+            //Arrange
+            string objectToTest = "NotNull";
+            string parent = "Parent";
+
+            //Act
+            Action act = () => Check.RequireNotNull(objectToTest, nameof(objectToTest), parent);
+
+            //Assert
+            act.Should().NotThrow<TechnicalException>();
+        }
+    }
+}
---
+++ DSG/DSG.Common/Check.cs
@@ -0,0 +1,34 @@
+﻿using DSG.Common.Exceptions;
+using log4net;
+
+namespace DSG.Common
+{
+    public static class Check
+    {
+        private static ILog _logger;
+
+        internal static ILog Logger => _logger ?? LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public static void RequireInjected(object requiredObject, string objectName, string nameOfParentClass)
+        {
+            if (requiredObject == null)
+            {
+                string errorMessage = $"The {objectName} was not correctly injected into {nameOfParentClass}.";
+
+                Logger.Error(errorMessage);
+                throw new TechnicalException(errorMessage);
+            }
+        }
+
+        public static void RequireNotNull(object requiredObject, string objectName, string nameOfParentClass)
+        {
+            if (requiredObject == null)
+            {
+                string errorMessage = $"The {objectName} in {nameOfParentClass} is null but must not be.";
+
+                Logger.Error(errorMessage);
+                throw new TechnicalException(errorMessage);
+            }
+        }
+    }
+}
